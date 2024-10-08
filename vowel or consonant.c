ccb356f448c39a8707ecdd8388a726e4863fee4f
Raj Narayan <hazrarajnarayan55@gmail.com>
2024-10-08 15:18:07 +0530

    vowel or consonant.c

--- vowel or consonant.c
+++ vowel or consonant.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
- int main(){
- char input;
- char array[]={'a','e','i','o','u'};
- printf("enter the alphabet:");
- scanf("%c",&input);
- for(int i=0;i<5;i++)
- {
-     if(input==array[i]){
-        printf("vowel");
-        goto end;}
-
-     }
-printf("consonant.");
-
-end:
-   return 0;
- }
+int main()
+{
+    char ch;
+    printf("write a letter: ");
+    scanf("%c",&ch);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+    printf("%c is a vowel",ch);
+}
+else{
+    printf("%c is a consonant",ch);
+}
+return 0;
+}
