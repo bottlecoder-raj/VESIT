6c89f94313a89a3d8a1dae6efc926c0065605faa
Raj Narayan <hazrarajnarayan55@gmail.com>
2024-11-13 22:28:28 +0530

    Create gcd.c

---
+++ EXP-05/gcd.c
@@ -0,0 +1,23 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int gcd(int *a,int *b)
+{
+    int t;
+    while(*b!=0)
+    {
+        t = *a % *b;
+
+        *a = *b;
+        *b = t;
+    }
+    return a;
+}
+int main() {
+    int a,b;
+    printf("Enter the 1st no: ");
+    scanf("%d",&a);
+    printf("Enter the 2nd no: ");
+    scanf("%d",&b);
+    gcd(&a,&b);
+    printf("Greatest common divisor is:%d",a);
+}
